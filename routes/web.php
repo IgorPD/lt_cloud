1272da088d82f87b8d8a3f45bffc8cfa50507e69
igor.domingues <igor.p.domingues@gmail.com>
2025-03-08 03:37:42 -0300

    feat: Adiciona rotas iniciais

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Rotas de autenticação 
+Route::middleware('guest')->group(function () {
+
+    Route::get('/login', function () {
+        return view('login');
+    })->name('login');
+
+    Route::post('/login', [LoginController::class, 'authenticate']);
+
+    Route::get('/register', function () {
+        return view('register');
+    })->name('register');
+
+    Route::post('/register', [RegisterController::class, 'store']);
 });
+
+
+// Logout disponível apenas para usuários logados
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+});
+
+
