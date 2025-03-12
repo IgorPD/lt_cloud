4e27b7fddfbdbadacbac3cfbc35d9b2fc9f22078
igor.domingues <igor.p.domingues@gmail.com>
2025-03-12 01:07:35 -0300

    feat: adiciona métodos para obter biografia e nome curtos de autores

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Author extends Model
 {
@@ -14,5 +15,15 @@ class Author extends Model
     public function articles()
     {
         return $this->belongsToMany(Article::class, 'article_author');
+    }
+
+    public function getShortBioAttribute()
+    {
+        return Str::limit($this->bio, 100, '...');
+    }
+
+    public function getShortNameAttribute()
+    {
+        return Str::limit($this->name, 50, '...');
     }
 }
