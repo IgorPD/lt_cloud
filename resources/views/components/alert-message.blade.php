63c4b797b6882e4c30b819889806d41554fd9af1
igor.domingues <igor.p.domingues@gmail.com>
2025-03-12 01:06:34 -0300

    feat: adiciona suporte a localização em português

--- resources/views/components/alert-message.blade.php
+++ resources/views/components/alert-message.blade.php
@@ -1,19 +1,19 @@
 @if(session('success'))
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
+    <div class="alert alert-success alert-dismissible fade show position-absolute" role="alert">
         {{ session('success') }}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
 
 @if(session('error'))
-    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <div class="alert alert-danger alert-dismissible fade show position-absolute" role="alert">
         {{ session('error') }}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
 
 @if ($errors->any())
-    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <div class="alert alert-danger alert-dismissible fade show position-absolute w-100" style="z-index: 100" role="alert">
         <ul class="mb-0">
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
